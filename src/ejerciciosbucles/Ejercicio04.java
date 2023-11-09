bedd35458a2bf805a77876f26cb664d95c79131a
jlmejorada <joseluis.mejorada@iesnervion.es>
2023-11-09 13:49:25 +0100

    UPD Ej4

--- src/ejerciciosbucles/Ejercicio04.java
+++ src/ejerciciosbucles/Ejercicio04.java
@@ -4,10 +4,15 @@ import java.util.InputMismatchException;
 import java.util.Scanner; 
 
 public class Ejercicio04 {
-
+	/*
+	 * ENTRADA: -2 | REL. ESPERADO: ERROR | REL. OBTENIDO: ERROR
+	 * ENTRADA: cuatro | REL. ESPERADO: ERROR | REL. OBTENIDO: ERROR
+	 * ENTRADA: num1=8 num2=12 | REL. ESPERADO: 4 | REL. OBTENIDO: 4
+	 * ENTRADA: num1=100 num2=50 | REL. ESPERADO: 50 | REL. OBTENIDO: 50
+	 */
 	public static void main(String[] args) {
-		//Creamos dos variables para los números introducidos y el número maximo de estos dos
-		int num1=-1, num2=-1, nummax;
+		//Creamos dos variables para los números introducidos y el número mínimo de estos dos
+		int num1=-1, num2=-1, nummin;
 		
 		//Creamos el escaner
 		Scanner sc = new Scanner(System.in);
@@ -42,14 +47,16 @@ public class Ejercicio04 {
 		
 		//Vemos que número es el menor
 		if (num1<num2) {
-			nummax=num1;
-		} else nummax=num2;
+			nummin=num1;
+		} else nummin=num2;
 		
 		// Se realiza el bucle hasta que se encuentra el M.C.D. de ambos números
-		while(num1%nummax!=0||num2%nummax!=0){
-			nummax--;
+		while(num1%nummin!=0||num2%nummin!=0){
+			nummin--;
 		} 
-		System.out.println("El máximo comun divisor es " + nummax);
+		
+		//Mostramos cual es el máximo comun divisor
+		System.out.println("El máximo comun divisor es " + nummin);
 		
 		//Cerramos el escaner
 		sc.close();
